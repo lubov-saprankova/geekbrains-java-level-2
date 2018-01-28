3087f807004843d0a1d4e1f97cf03f883d93b618
decilya <decilya@gmail.com>
2018-01-28 19:28:13 +0300

    lesson 2

---
+++ lesson_2/src/ilya_v/java2/lesson_2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ilya_v.java2.lesson_2;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
