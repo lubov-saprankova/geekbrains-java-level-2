12ea84c3f6c1e5a13ece6a3f6a74d70577730e4f
decilya <decilya@gmail.com>
2018-01-28 19:07:44 +0300

    lesson 2

--- lesson_2/src/ilya_v/java2/lesson_2/Main.java
+++ lesson_2/src/ilya_v/java2/lesson_2/Main.java
@@ -1,12 +1,29 @@
 package ilya_v.java2.lesson_2;
 
+import java.util.Arrays;
+
 public class Main {
 
+    /**
+     * 1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
+     при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+
+     * @param arr Строковый массив 4x4
+     */
+    private static void twoStringArr(String[][] arr) {
+
+        if ((arr.length != 4) || (arr[0].length != 4)) throw new MyArraySizeException("Необходим массив размером 4х4");
+
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+
     public static void main(String[] args) {
+        String[][] t = new String[4][5];
+        twoStringArr(t);
 
         /*
-         1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
-        при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+
 
         2. Далее метод должен пройтись по всем элементам массива, преобразовать в int,
             и просуммировать. Если в каком-то элементе массива преобразование не удалось (например,
---
+++ lesson_2/src/ilya_v/java2/lesson_2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ilya_v.java2.lesson_2;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message){
+        super(message);
+    }
+}
