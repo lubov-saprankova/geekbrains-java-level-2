7651b8af4353aff2f46f5c6893e9795c15aedeeb
decilya <decilya@gmail.com>
2018-02-02 01:36:02 +0300

    start lesson 3

--- lesson_3/src/ilya_v/java2/lesson_3/Main.java
+++ lesson_3/src/ilya_v/java2/lesson_3/Main.java
@@ -1,13 +1,49 @@
 package ilya_v.java2.lesson_3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+
+    public static int calcWord(ArrayList<String> arrList, String word) {
+        return Collections.frequency(arrList, word);
+    }
 
     public static void main(String[] args) {
 
-        /*
-           1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+        /*  1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
            Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
-           Посчитать сколько раз встречается каждое слово.
+           Посчитать сколько раз встречается каждое слово. */
+
+        ArrayList<String> wordList = new ArrayList<>();
+        wordList.add("умаять");
+        wordList.add("подвздошный");
+        wordList.add("разъясняться");
+        wordList.add("лечить");
+        wordList.add("лечить");
+        wordList.add("фарцовщица");
+        wordList.add("гудронный");
+        wordList.add("петербуржец");
+        wordList.add("безалкогольный");
+        wordList.add("фарцовщица");
+        wordList.add("прилипало");
+        wordList.add("притворство");
+        wordList.add("умаять");
+        wordList.add("гудронный");
+        wordList.add("раскислый");
+        wordList.add("танцор");
+        wordList.add("окказионалист");
+        wordList.add("притворство");
+        wordList.add("умелица");
+        wordList.add("гудронный");
+
+        for (int i = 0; i < wordList.size(); i++) {
+            if (calcWord(wordList, wordList.get(i)) == 1) {
+                System.out.println(wordList.get(i));
+            }
+        }
+
+        /*
 
            2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
            В этот телефонный справочник с помощью метода add() можно добавлять записи.
@@ -21,6 +57,6 @@ public class Main {
             Консоль желательно не использовать (в том числе Scanner),
             тестировать просто из метода main() прописывая add() и get().
         */
-        
+
     }
 }
