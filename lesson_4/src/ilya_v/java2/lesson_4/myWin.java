6798d76b8b5e6a3b4e50a4c37dddb311fdf714e7
via <via@wizardforum.ru>
2018-02-06 17:09:20 +0300

    final lesson_4

--- lesson_4/src/ilya_v/java2/lesson_4/myWin.java
+++ lesson_4/src/ilya_v/java2/lesson_4/myWin.java
@@ -3,39 +3,66 @@ package ilya_v.java2.lesson_4;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 class myWin extends JFrame {
 
-    myWin(){
+    private MyHintTextField myJPlaceholder;
+    private JTextArea myTextArea;
+
+    class MyHintTextField extends JTextField {
+        String hint;
+
+        public MyHintTextField(String hint) {
+            this.hint = hint;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (getText().isEmpty()) {
+                g.drawString(hint, 4, 16);
+            }
+        }
+    }
+
+    myWin() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(550, 260, 666, 666);
-        setLayout(new FlowLayout());
+        setTitle("Мой \"Чат\" на Java (Swig)");
+        setBounds(1100, 500, 400, 400);
+        JButton jBtn = new JButton("ok");
 
-        JButton jBtn1 = new JButton("Button #1");
-        JButton jBtn2 = new JButton("Button #2");
+        myJPlaceholder = new MyHintTextField("Напишите сообщение...");
+        myTextArea = new JTextArea(0, 0);
+        
+        myTextArea.setLineWrap(true);
+        myTextArea.setEditable(false);
+        
+        JScrollPane jsp = new JScrollPane(myTextArea);
+        add(jsp, BorderLayout.CENTER);
 
-        add(jBtn1);
-        add(jBtn2);
-
-        jBtn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("yo!");
-            }
+        jBtn.addActionListener((ActionEvent e) -> {
+            sendMessage();
         });
 
+        myJPlaceholder.addActionListener(e -> sendMessage());
 
-        JTextField jTextInput = new JTextField(20);
-        add(jTextInput);
-
-        jBtn2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(jTextInput.getText());
-            }
-        });
+        JPanel upperPanel = new JPanel(new BorderLayout());
+        upperPanel.add(jBtn, BorderLayout.EAST);
+        upperPanel.add(myJPlaceholder, BorderLayout.CENTER);
+        add(upperPanel, BorderLayout.NORTH);
+        Font font = new Font("Roboto", Font.PLAIN, 16);
+        myTextArea.setFont(font);
 
         setVisible(true);
+    }
+
+    void sendMessage() {
+
+        if (!myJPlaceholder.getText().isEmpty()) {
+            myTextArea.append(myJPlaceholder.getText() + "\n");
+        }
+
+        myJPlaceholder.setText("");
+        myJPlaceholder.grabFocus();
     }
 }
