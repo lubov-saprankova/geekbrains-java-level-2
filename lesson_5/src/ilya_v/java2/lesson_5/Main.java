209a535fc9c5f9678013600e1a721c0a1af6e321
via <via@wizardforum.ru>
2018-02-12 18:51:10 +0300

    fix

--- lesson_5/src/ilya_v/java2/lesson_5/Main.java
+++ lesson_5/src/ilya_v/java2/lesson_5/Main.java
@@ -1,18 +1,21 @@
 package ilya_v.java2.lesson_5;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
-    static float[] createArr(int size){
+    final static int THREADS_COUNT = 4;
+
+    static float[] createArr(int size) {
 
         // 1. Создают одномерный длинный массив, например:
         float[] arr = new float[size];
 
         // 2. Заполняют этот массив единицами.
-        for (int i = 0; i < arr.length; i++){
-                arr[0] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[0] = 1;
         }
 
         return arr;
@@ -25,8 +28,8 @@ public class Main {
         long a = System.currentTimeMillis();
 
         // 4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
         /*
@@ -35,9 +38,43 @@ public class Main {
         return (System.currentTimeMillis() - a);
     }
 
+
     static long reSetArrInTwoThread(float[] arr) {
 
+        int countElementForThread = (arr.length / THREADS_COUNT);
+
+        Arrays.fill(arr, 1f);
+        long a = System.currentTimeMillis();
+        float[][] m = new float[THREADS_COUNT][countElementForThread];
+        Thread[] t = new Thread[THREADS_COUNT];
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            System.arraycopy(arr, countElementForThread * i, m[i], 0, countElementForThread);
+            final int u = i; //обычную переменную нельзя передать в поток
+            t[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0, z = u * countElementForThread; j < countElementForThread; j++, z++) {
+                        m[u][j] = (float) (m[u][j] * Math.sin(0.2f + z / 5) * Math.cos(0.2f + z / 5) * Math.cos(0.4f + z / 2));
+                    }
+                }
+            });
+
+            t[i].start();
+        }
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            System.arraycopy(m[i], 0, arr, i * countElementForThread, countElementForThread);
+        }
+
+        return System.currentTimeMillis() - a;
     }
+
 
     public static void main(String[] args) {
 
@@ -49,45 +86,8 @@ public class Main {
             ● Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и
             потом склеивает эти массивы обратно в один. */
 
-
-
-
-        /* 1. Необходимо написать два метода, которые делают следующее:
-        1) Создают одномерный длинный массив, например:
-
-        static final int size = 10000000;
-        static final int h = size / 2;
-        float[] arr = new float[size];*/
-
-
-
-
-
-       /* MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
-
-        t1.start();
-        t2.start();
-
-        Thread t3 = new Thread(new MyRunnable() {
-            @Override
-            public void run() {
-                super.run();
-            }
-        });
-
-        t3.start();
-
-        ExecutorService service = Executors.newFixedThreadPool(55);
-
-        for (int i = 0; i < 55; i++) {
-            int x = i;
-            service.execute(() -> System.out.println("A" + i));
-        }
-
-        service.shutdown();
-
-        new Thread(() -> { System.out.println("yo");}).start();*/
+        float[] arrNew = createArr(10_000_000);
+        reSetArrInTwoThread(arrNew);
 
 
     }
