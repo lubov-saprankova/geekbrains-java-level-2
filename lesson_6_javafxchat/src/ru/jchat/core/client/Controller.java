ffd94739b8f47fe9baf11fffca7f6083144999d5
decilya <decilya@gmail.com>
2018-02-13 15:48:52 +0300

    lesson 6 fx

--- lesson_6_javafxchat/src/ru/jchat/core/client/Controller.java
+++ lesson_6_javafxchat/src/ru/jchat/core/client/Controller.java
@@ -1,4 +1,43 @@
 package ru.jchat.core.client;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField msgField;
+
+    private Socket socket;
+    private DataOutputStream  out;
+    private DataInputStream in;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void sendMessage(){
+
+        try {
+            out.writeUTF(msgField.getText());
+            msgField.clear();
+            msgField.requestFocus();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
